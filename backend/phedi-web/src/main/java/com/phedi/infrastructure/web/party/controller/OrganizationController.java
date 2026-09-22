440724a7500870a03c135a51db430ff836d86b7f
Adilson Krasovski <adilson.krasovski@gmail.com>
2026-09-22 14:56:08 -0300

    Reorganização dos metodos, aplicando os metodos corretos

--- backend/phedi-web/src/main/java/com/phedi/infrastructure/web/party/controller/OrganizationController.java
+++ backend/phedi-web/src/main/java/com/phedi/infrastructure/web/party/controller/OrganizationController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +52,7 @@ public class OrganizationController {
         return ResponseEntity.created(location).build();
     }
 
-    @PutMapping("/{partyIdentifier}")
+    @PatchMapping("/{partyIdentifier}")
     public ResponseEntity<Void> update(@PathVariable PartyIdentifier partyIdentifier, @RequestBody UpdateOrganizationRequest updatedOrganization){
         var domain = mapper.toDomain(partyIdentifier.value(), updatedOrganization);
         organizationService.update(domain);
@@ -65,13 +65,13 @@ public class OrganizationController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{partyIdentifier}/activate")
+    @PostMapping("/{partyIdentifier}/activate")
     public ResponseEntity<Void> activate(@PathVariable PartyIdentifier partyIdentifier){
         organizationService.activate(partyIdentifier);
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{partyIdentifier}/deactivate")
+    @PostMapping("/{partyIdentifier}/deactivate")
     public ResponseEntity<Void> deactivate(@PathVariable PartyIdentifier partyIdentifier){
         organizationService.deactivate(partyIdentifier);
         return ResponseEntity.noContent().build();
